97a0951295d18c9be07eeb5244e271c1fcea706e
zsaffaapp <zsaffaaulia@mhs.unida.gontor.ac.id>
2020-11-30 15:21:27 +0700

    project pertamaku

---
+++ home.php
@@ -0,0 +1,20 @@
+<?php //tag
+echo "welcome guys";
+echo "iz*one official fancafe";
+
+$a = 1;
+$nama = "IZ*ONE";
+
+echo "<h1>IZ*ONE Comeback, 7th December</h1>", $nama;
+?>
+
+<?php
+
+
+
+for($i=0; $i<10; $i++)
+{
+    echo "<h1>IZ*ONE COMEBACK ONE-REELER</h1>";
+}
+
+?>
